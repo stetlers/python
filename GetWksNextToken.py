a1bc9d04c45251d7d2cdbb424bb5806e2ce58827
stetlers <stetlers@amazon.com>
2021-01-08 12:57:16 -0500

    Fixed multiple issues from AP comments
    Changed typos from quick re-write into class, fixed logic for comparing the images by introducting a list.

--- GetWksNextToken.py
+++ GetWksNextToken.py
@@ -2,11 +2,14 @@
 Perform lookups on my AWS Account using a module
 """
 
-import helpermodule_amazonworkspaces
+#import helpermodule_amazonworkspaces
+import helpermodule_amznwksimagecopy
+copy = helpermodule_amznwksimagecopy.ImageCopyHelper('us-east-1','eu-west-2')
+print(copy.copy_images())
 
-DIRECTORY = 'd-9067615ecb'
-WORKSPACE = 'ws-hfk1xk03b'
-INFO = helpermodule_amazonworkspaces.WorkspaceHelper('us-east-1')
-print(INFO.get_dir_wks(DIRECTORY))
+#DIRECTORY = 'd-9067615ecb'
+#WORKSPACE = 'ws-hfk1xk03b'
+#INFO = helpermodule_amazonworkspaces.WorkspaceHelper('us-east-1')
+#print(INFO.get_dir_wks(DIRECTORY))
 
-print(INFO.get_info(WORKSPACE))
+#print(INFO.get_info(WORKSPACE))
--- helpermodule_amznwksimagecopy.py
+++ helpermodule_amznwksimagecopy.py
@@ -5,47 +5,55 @@ Author: Stephen K Stetler
 import boto3
 
 class ImageCopyHelper():
-    """ Class to copy custom images created by administrator using the private API between AWS Regions """
+    # Class to copy custom images created by administrator using the private API between AWS Regions 
     def __init__(self,src_region,dst_region):
-        """ Init attributes for info collection """
-        self.src_region = src_region
-        self.dst_region = dst_region
-        self.src_client = boto3.client(
+        # Init attributes for info collection 
+        self.src_region=src_region
+        self.dst_region=dst_region
+        self.src_client=boto3.client(
             'workspaces',
-            src_region_name = self.src_region
+            region_name = self.src_region
         )
-        self.dst_client = boto3.client(
+        self.dst_client=boto3.client(
             'workspaces',
-            dst_region_name = self.dst_region
+            region_name = self.dst_region
         )
     
     def copy_images(self):
-        """ Gather lists for all source and destination Custom Images """
+        # Gather lists for all source and destination Custom Images 
         all_src_images = self.src_client.describe_workspace_images()
         all_dst_images = self.dst_client.describe_workspace_images()
-        for src_img in all_src_images:
-            """ Check if the image exists in the destination region. """
-            if src_img not in all_dst_images:
-                """ Copy the image to the destination """
+        output = str('')
+        # We only keep the images names in the destination list because that's all we need
+        # and using a list is easier for the comparison
+        list_of_dst_images = []
+        for dst_img in all_dst_images['Images']:
+            list_of_dst_images.append(dst_img['Name'])
+        # Check source image names in the new list
+        for src_img in all_src_images['Images']:
+            # Check if the image exists in the destination list of names. 
+            if src_img['Name'] not in list_of_dst_images:
+                # Copy the image to the destination 
                 try:
                     self.dst_client.copy_workspace_image(
-                        Name=src_img[Name], 
+                        Name=src_img['Name'], 
                         Description='Copied through automation', 
-                        SourceImageId=src_img[ImageId], 
+                        SourceImageId=src_img['ImageId'], 
                         SourceRegion=self.src_region
                     )
-                    output += f'Copying {} to {}...'.format(
-                        scr_img[ImageId], 
-                        self.src_region
+                    output += 'Copying {} to {}...\n'.format(
+                        src_img['ImageId'], 
+                        self.dst_region
                     )
                 except Exception as error:
-                    output += f'Copy failed for {}'.format(
-                        src_img[ImageId]
+                    output += 'Copy failed for {}: {}\n'.format(
+                        src_img['ImageId'],
+                        error
                     )
             else:
-                output += f'Nothing to copy from {} to {}'.format(
+                output += 'Nothing to copy from {} to {}\n'.format(
                     self.src_region,
                     self.dst_region
                 )
-    return output
+        return output
  
